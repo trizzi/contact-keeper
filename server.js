4a97ff84bed21d3c97c1a2a6b16e918bc7ecc4c1
Ric <ric@Doom-Machine.local>
2021-10-19 06:01:54 +0100

    Prepare for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 // connect Database
@@ -7,14 +8,26 @@ connectDB();
 // init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) =>
-	res.json({ msg: 'welcome to the contactKeeper API...' })
-);
-
 // define routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+	// set static asset folder
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) =>
+		res.sendFile(
+			path.resolve(
+				__dirname,
+				'client',
+				'build',
+				'index.html'
+			)
+		)
+	);
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
